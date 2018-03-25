ad590edc9b738d96e7b240dfecc7bb5b6f9ac9fc
sasivardhan518 <sasivardhan518@gmail.com>
2018-03-25 19:41:39 +0530

    object two way binding

--- src/child/child.component.ts
+++ src/child/child.component.ts
@@ -1,5 +1,6 @@
 import {Input, Output, EventEmitter} from '@angular/core';
 import { Component, OnInit } from '@angular/core';
+import { User } from '../models/User';
 
 @Component({
   selector: 'app-child',
@@ -7,11 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./child.component.css']
 })
 export class ChildComponent implements OnInit {
+  @Input() userFromParent: User;
   @Input() messageFromParent: string;
-  @Output() messageFromParentChange= new EventEmitter<string>();
-  constructor() { }
+  // @Output() messageFromParentChange= new EventEmitter<string>();
+  constructor() {
+    this.userFromParent = new User();
+  }
 
   ngOnInit() {
   }
 
+  submit() {
+    console.log(this.userFromParent);
+  }
 }
--- src/parent/parent.component.ts
+++ src/parent/parent.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../models/User';
 
 @Component({
   selector: 'app-parent',
@@ -7,13 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ParentComponent implements OnInit {
 message: string;
+user: User;
   constructor() {
     this.message = 'Hello world!';
+    this.user = new User();
+    this.user.userName = 'shashi';
+    this.user.age = 25;
+    this.user.gender = 'male';
   }
 
   ngOnInit() {
   }
   messageFromParentChanged(message: string) {
      this.message = message;
+  }
+
+  submit() {
+    console.log(this.user);
   }
 }
